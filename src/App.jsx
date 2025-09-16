e78727af04b0e6a9247208fbcc950ec93c0fb01d
Christian Kodehode <christian404.kodehode@gmail.com>
2025-09-16 09:24:01 +0200

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import meny from "./data/meny.jsx";
+import menyen from "./components/menyen.jsx";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Restaurant Meny</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
